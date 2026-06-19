6a6537a5f50032b8a600b80ee28c68339f416c8a
Thet97 <thethtut99@gmail.com>
2026-06-20 01:21:52 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,14 +2,17 @@ import os
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+TOKEN = os.getenv("BOT_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
         "🎬 Welcome To ReelVerse MM\n\nSend a movie name."
     )
 
-app = Application.builder().token(BOT_TOKEN).build()
-app.add_handler(CommandHandler("start", start))
+def main():
+    app = Application.builder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    app.run_polling()
 
-app.run_polling()
+if __name__ == "__main__":
+    main()
