5ef757b7f5b07adce21da87e7f5223d08ab00ab4
Thet97 <thethtut99@gmail.com>
2026-06-20 01:36:56 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,18 +1,17 @@
 import os
+import asyncio
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 
 TOKEN = os.getenv("BOT_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(
-        "🎬 Welcome To ReelVerse MM\n\nSend a movie name."
-    )
+    await update.message.reply_text("🎬 Welcome To ReelVerse MM")
 
-def main():
+async def main():
     app = Application.builder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
-    app.run_polling()
+    await app.run_polling()
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
